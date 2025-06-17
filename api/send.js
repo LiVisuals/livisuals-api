c6837937b44dea0f905fc5bbcbe268085cf132e8
Pradhumya <pradhumya@carltonleisure.com>
2025-06-17 17:23:26 +0100

    Fix: Enable CORS for livisuals.co.uk contact form

--- api/send.js
+++ api/send.js
@@ -1,10 +1,19 @@
 const sgMail = require('@sendgrid/mail');
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = async (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(405).end('Method Not Allowed');
+    // Set CORS headers
+    res.setHeader("Access-Control-Allow-Origin", "https://livisuals.co.uk");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Handle preflight OPTIONS request
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    if (req.method !== "POST") {
+        return res.status(405).end("Method Not Allowed");
     }
 
     const { name, email, phone, eventDate, sessionType, heardFrom, message } = req.body;
@@ -19,7 +28,7 @@ module.exports = async (req, res) => {
       <b>Phone:</b> ${phone}<br>
       <b>Session Type:</b> ${sessionType}<br>
       <b>Event Date:</b> ${eventDate}<br>
-      <b>Heard About Us From:</b> ${heardFrom}<br><br>
+      <b>Heard About Us From:</b> ${heardFrom}<br>
       <b>Message:</b><br>${message}
     `
     };
